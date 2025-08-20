110659ca6ee5d721d9e10e0d1ebcda9eb55fe6a7
raja00201 <rajaboopathi584@gmail.com>
2025-08-20 14:37:35 +0100

    Model_Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -86,7 +86,11 @@ class DataTransformation:
                 obj=preprocessing_obj
             )
 
-            return train_arr, test_arr, self.data_transformation_config.preprocessor_obj_file_path
-
+            return (
+                   train_arr, 
+                   test_arr, 
+                   self.data_transformation_config.preprocessor_obj_file_path
+            )
+        
         except Exception as e:
             raise CustomException(e, sys)
