299dc2c28af67e989c9a39c7eb06a32b617d5efe
raja00201 <rajaboopathi584@gmail.com>
2025-08-25 16:50:50 +0100

    new info

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,12 @@
 import os
 import sys
+import pickle
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+
+
 def save_object(file_path, obj):
     """
     Save a Python object to a file using dill.
@@ -26,7 +29,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             para = param[list(models.keys())[i]]
             gs = GridSearchCV(model,para,cv=3)
             gs.fit(X_train,y_train)
-
+ 
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
@@ -44,6 +47,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
